ecb51b07c4041724a399cfa846ba9d1013de906c
Gonzalo <gonzalo.bide@gmail.com>
2017-11-29 02:12:30 -0300

    hanlding movement

--- src/App.js
+++ src/App.js
@@ -13,7 +13,22 @@ const DUNGEON_CONFIG = {
 }
 const TILES = [ ]
 const MOVE_KEYS = [
-  {}
+  {
+    code: 37,
+    positionFunc: pos => [pos[0], pos[1] - 1]
+  },
+  {
+    code: 38,
+    positionFunc: pos => [pos[0] - 1, pos[1]]
+  },
+  {
+    code: 39,
+    positionFunc: pos => [pos[0], pos[1] + 1]
+  },
+  {
+    code: 40,
+    positionFunc: pos => [pos[0] + 1, pos[1]]
+  }
 ]
 // Fin config
 
@@ -30,14 +45,13 @@ const normalizeDungeon = dungeon =>
   )
 
 const loadDungeonElements = (dungeonMap, playerPosition) =>
-  overMapCell(dungeonMap, (cell, rowIndex, colIndex) => {
-    if(cell === 1) return cell
-    if(playerPosition[0] === rowIndex && playerPosition[1] === colIndex)
-      return 2
-    if(Math.random()*10<9.975)
-      return 0
-    else return 3
-  })
+  overMapCell(dungeonMap, (cell, rowIndex, colIndex) =>
+    (cell === 1) ? cell :
+      (playerPosition[0] === rowIndex && playerPosition[1] === colIndex) ? 2 :
+        (Math.random()<.9965) ? 0 :
+          (Math.random()<0.3) ? 3 : 4
+  )
+
 const findFirstEmptyPosition = dungeon => {
   var rowNumber;
   var cellNumber;
@@ -49,7 +63,7 @@ const findFirstEmptyPosition = dungeon => {
   })
   return [ rowNumber, cellNumber ]
 }
-  
+
 
 class App extends Component {
   constructor() {
@@ -63,13 +77,31 @@ class App extends Component {
       dungeon: loadedDungeon,
       playerPosition: initPlayerPosition,
     }
+
+    this.handleMovement = this.handleMovement.bind(this)
   }
+
+  handleMovement(event) {
+    const keyCode = event.keyCode;
+    const currentMove = MOVE_KEYS.find(m => m.code === keyCode);
+    if (!currentMove) return;
+  
+    this.movePlayer(
+      currentMove.positionFunc(this.state.playerPosition)
+    );
+  }
+  movePlayer(position) {
+    this.setState( {
+      playerPosition: [position[0], position[1]]
+    })
+  }
+
   render() {
     return (
-      <div className="App">
+      <div className="App" onKeyDown={this.handleMovement} tabIndex="1">
         {this.state.dungeon.map((row, rowIndex) => (
           <div className="dungeon-row">
-            {row.map( cell => <div className={`dungeon-cell ${ (cell===0) ? 'empty' : (cell===1)?'wall': (cell===2)? 'player': 'food' }`} />)}
+            {row.map( cell => <div className={`dungeon-cell ${ (cell===0) ? 'empty' : (cell===1)?'wall': (cell===2)? 'player': (cell===3) ?'food' : 'enemy' }`} />)}
           </div>
         ))}
         {this.state.playerPosition}
